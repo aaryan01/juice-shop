48263f093ee477048a3ce57ec4cc669c8e18b813
aaryan10 <aaryanbudhiraja2@gmail.com>
2019-04-15 21:48:20 +0530

    orders

--- routes/dataExport.js
+++ routes/dataExport.js
@@ -31,31 +31,32 @@ module.exports = function dataExport () {
           })
           userData.orders = orders
         }
+
+        db.reviews.find({ $where: "this.author === '" + email + "'" }).then(reviews => {
+          const result = utils.queryResultToJson(reviews)
+          const data = result.data
+          if (data.length > 0) {
+            let reviews = []
+            data.map(review => {
+              let finalReview = {
+                message: review.message,
+                author: review.author,
+                productId: review.product,
+                likesCount: review.likesCount,
+                likedBy: review.likedBy
+              }
+              reviews.push(finalReview)
+            })
+            userData.reviews = reviews
+          }
+          res.status(200).send({ userData: JSON.stringify(userData, null, 2), confirmation: 'Your data export will open in a new Browser window.' })
+        },
+        () => {
+          next(new Error(`Error retrieving reviews for ${updatedEmail}`))
+        })
       },
       () => {
         next(new Error(`Error retrieving orders for ${updatedEmail}`))
-      })
-      db.reviews.find({ $where: "this.email === '" + updatedEmail + "'" }).then(reviews => {
-        const result = utils.queryResultToJson(reviews)
-        const data = result.data
-        if (data.length > 0) {
-          let reviews = []
-          data.map(review => {
-            let finalReview = {
-              message : review.message,
-              author : review.author,
-              productId : review.product,
-              likesCount : review.likesCount,
-              likedBy : review. likedBy
-            }
-            reviews.push(finalReview)
-          })
-          userData.reviews = reviews
-        }
-        res.status(200).send({ userData: JSON.stringify(userData, null, 2), confirmation: 'Your data export will open in a new Browser window.' })
-      },
-      () => {
-        next(new Error(`Error retrieving reviews for ${updatedEmail}`))
       })
     } else {
       next(new Error('Blocked illegal activity by ' + req.connection.remoteAddress))
